7f217db81f841f001ee9d0798b82c54e274f5f7b
OmaFineseed <favourfineseed@gmail.com>
2022-06-23 12:52:38 +0100

    second commit: checking if user input is not a number

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
-action = prompt ('what operation whould you like to perform +, -, *, /');
+action = prompt ('what operation whould you like to perform  +,  -,  *,  /');
 let num1, num2;
-num1 = (parseFloat(parseInt(prompt('Your first number'))));
-num2 = (parseFloat(parseInt(prompt('Your second number'))));
-if (action == "+" ) {
-    let a = num1 + num2
+ 
+num1 = parseFloat(parseInt(prompt('Your first number')));
+num2 = parseFloat(parseInt(prompt('Your second number')));
+
+if (isNaN (num1) || isNaN (num2)){
+    alert("You entered a wrong choice");
+}
+else if (action == "+" ) {
+    a = num1 + num2
 alert(a, " ", "is the sum of the numbers you chose");
 
 }
 else if (action == "-" ) {
-    let s = num1 - num2
+   s = num1 - num2
         
-alert(s, " ", "is the differnce of the numbers you chose");
+alert(s, "is the difference of the numbers you chose");
 }
  
 
 else if (action == "*") {
-     let m = num1 * num2
+     m = num1 * num2
 alert(m, " ", "is the product of the numbers you chose");
 }
 
 else if (action == "/") {
-   let d = num1 / num2
+   d = num1 / num2
 alert(d, " ", "is the quotient of the numbers you chose");
 }
+ 
  
